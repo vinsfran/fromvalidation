7deb899dfc1e22b582ff5b8ceb515acedb106483
Vicente Insfran <vicente.insfran@asuncion.gov.py>
2021-03-26 11:59:09 -0300

    196. Inicio de proyecto - FormulariosBloc

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+import 'package:formvalidation/src/pages/login_page.dart';
+import 'package:formvalidation/src/pages/home_page.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Material App',
+      initialRoute: LoginPage.routeName,
+      routes: {
+        LoginPage.routeName: (BuildContext context) => LoginPage(),
+        HomePage.routeName: (BuildContext context) => HomePage(),
+      },
+    );
+  }
+}
---
+++ lib/src/pages/login_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  static final String routeName = 'login';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Login Page'),
+      ),
+    );
+  }
+}
