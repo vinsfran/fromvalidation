89c3940c92e7ff3b7f7fa45011af73f2dca486ae
Vicente Insfran <vicente.insfran@asuncion.gov.py>
2021-04-05 15:01:41 -0400

    201. InheritedWidget

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:formvalidation/src/blocs/provider.dart';
 
 import 'package:formvalidation/src/pages/login_page.dart';
 import 'package:formvalidation/src/pages/home_page.dart';
@@ -8,14 +9,16 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Material App',
-      initialRoute: LoginPage.routeName,
-      routes: {
-        LoginPage.routeName: (BuildContext context) => LoginPage(),
-        HomePage.routeName: (BuildContext context) => HomePage(),
-      },
+    return Provider(
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Material App',
+        initialRoute: LoginPage.routeName,
+        routes: {
+          LoginPage.routeName: (BuildContext context) => LoginPage(),
+          HomePage.routeName: (BuildContext context) => HomePage(),
+        },
+      ),
     );
   }
 }
---
+++ lib/src/blocs/provider.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+import 'package:formvalidation/src/blocs/login_bloc.dart';
+
+class Provider extends InheritedWidget {
+  final loginBloc = LoginBloc();
+
+  Provider({Key key, Widget child}) : super(key: key, child: child);
+
+  @override
+  bool updateShouldNotify(covariant InheritedWidget oldWidget) => true;
+
+  static LoginBloc of(BuildContext context) {
+    //return (context.inheritedWidgetOfExactType(Provider) as Provider).loginBloc;
+    return context.dependOnInheritedWidgetOfExactType<Provider>().loginBloc;
+  }
+}
